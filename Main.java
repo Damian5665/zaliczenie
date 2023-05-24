3dc254016ffbad0718b06fc9d7e83d2cb8c4c5f3
Damian5665 <126695965+Damian5665@users.noreply.github.com>
2023-05-24 14:14:52 +0000

    dziala

--- Main.java
+++ Main.java
@@ -1,5 +1,27 @@
 class Main {
   public static void main(String[] args) {
-    System.out.println("witaj git!");
+    int[] tab = new int[10];
+    int i;
+    int ip=1;
+    int inp=1;
+    System.out.println("Znaki w tablicy: ");
+    for(i=1;i<11;i++)
+      {
+        tab[i-1] = i * i;
+        System.out.print(tab[i-1] + " ");
+      }
+    for(i=0;i<10;i++)
+      {
+        if(tab[i]%2 == 0)
+        {
+          ip = ip * tab[i];
+        }
+        else
+        {
+          inp = inp * tab[i];
+        }
+      }
+    System.out.println("iloczyn liczb parzystych wynosi: " + ip);
+    System.out.println("iloczyn liczb nieparzystych wynosi: " + inp);
   }
 }
